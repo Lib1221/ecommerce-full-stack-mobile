0c0ad8496ee19ffdc4fce7cb0229760f38f2c075
Liben <adugnaliben65@gmail.com>
2025-07-18 18:26:47 +0300

    Add PostgreSQL database configuration to settings.py and create .env file for environment variables

--- backend/shopping_backend/settings.py
+++ backend/shopping_backend/settings.py
@@ -51,10 +51,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shopping_backend.wsgi.application'
 
 
+# Add these at the top of your settings.py
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
+
+load_dotenv()
+
+# Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
